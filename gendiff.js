ff6cc014d7849d076e0e25545c0967e72381b768
Kachulina Tatyana <kachulinatanya@yandex.ru>
2024-09-26 14:37:50 +0300

    add format option and arguments for command

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V --version', 'output the version number')
-  .helpOption('-h --help', 'output usage information');
-
-program.parse();
+  .helpOption('-h --help', 'output usage information')
+  .option('-f --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .parse();
